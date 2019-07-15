90a18275835f3a5b4c91a3c415dcfe4deb8d4287
Voraton Nuttakarnnuwat <riskmaster2468@gmail.com>
2019-07-15 16:50:30 +0700

    ปรับแก้ไข Logic ทำเป็น ng-for

--- src/app/dashboard/dashboard.page.ts
+++ src/app/dashboard/dashboard.page.ts
@@ -7,9 +7,20 @@ import { Component, OnInit } from '@angular/core';
 })
 export class DashboardPage implements OnInit {
 
+  card_arr = [
+    {subtitle:'',title:'ค่าใช้จ่าย เลี้ยงชีพ',img:'assets/card_1.jpg',detail:'เป็นจำนวนเงินทั้งหมด : 7,500 ฿'},
+    {subtitle:'',title:'',img:'assets/icon_2.png',detail:''},
+  ]
+
   constructor() { }
 
   ngOnInit() {
   }
+
+  // onClickCard(){
+
+  //   console.log('Click');
+
+  // }
 
 }
