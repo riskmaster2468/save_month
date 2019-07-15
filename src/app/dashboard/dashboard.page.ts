591b7bc9870cba29b1c463a10952593a69daf396
Voraton Nuttakarnnuwat <riskmaster2468@gmail.com>
2019-07-15 18:23:23 +0700

    เพิ่ม plugin Armchart ลงไป ในหน้า dash และทำการ เพิ่ม SCSS ลงใน dash และ ลอง Set Data แบบ for ลงใน Function Dashboard

--- src/app/dashboard/dashboard.page.ts
+++ src/app/dashboard/dashboard.page.ts
@@ -1,4 +1,12 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, NgZone } from '@angular/core';
+/* Imports */
+import * as am4core from "@amcharts/amcharts4/core";
+import * as am4charts from "@amcharts/amcharts4/charts";
+import am4themes_animated from "@amcharts/amcharts4/themes/animated";
+
+//Themes begin
+am4core.useTheme(am4themes_animated);
+// Themes end
 
 @Component({
   selector: 'app-dashboard',
@@ -8,19 +16,38 @@ import { Component, OnInit } from '@angular/core';
 export class DashboardPage implements OnInit {
 
   card_arr = [
-    {subtitle:'',title:'ค่าใช้จ่าย เลี้ยงชีพ',img:'assets/card_1.jpg',detail:'เป็นจำนวนเงินทั้งหมด : 7,500 ฿'},
-    {subtitle:'',title:'',img:'assets/icon_2.png',detail:''},
+    {subtitle:'',title:'ค่าใช้จ่าย เลี้ยงชีพ',img:'assets/card_1.jpg',detail:'เป็นจำนวนเงินทั้งหมด : ',value: 7500},
+    {subtitle:'',title:'',img:'assets/icon_2.png',detail:'',value: 17500},
   ]
 
-  constructor() { }
+  onClickCard(){
 
+    console.log('Click');
+
+  }
+  
   ngOnInit() {
   }
 
-  // onClickCard(){
+  private chart: am4charts.XYChart;
 
-  //   console.log('Click');
+  constructor(private zone: NgZone) {}
 
-  // }
+  ngAfterViewInit() {
+    this.zone.runOutsideAngular(() => {
+
+      let chart = am4core.create("chartdiv", am4charts.PieChart3D);
+      chart.hiddenState.properties.opacity = 0; // this creates initial fade-in
+
+      chart.legend = new am4charts.Legend();
+
+      chart.data = this.card_arr;
+
+      let series = chart.series.push(new am4charts.PieSeries3D());
+      series.dataFields.value = "value";
+      series.dataFields.category = "title";
+
+    });
+  }
 
 }
